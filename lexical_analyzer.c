6959d884955a67e4b69586355e9ffebce271e38b
shvwxyz <155872247+shvwxyz18@users.noreply.github.com>
2024-06-19 21:10:40 +0530

    Create lexical_analyzer.c

---
+++ lexical_analyzer.c
@@ -0,0 +1,42 @@
+%{
+#include<stdio.h>
+%}
+
+char [a-zA-Z]
+digit [0-9]
+id {char}+({char}|{digit})*
+fun {id}\(\)
+number {digit}+(\.{digit}+)*
+lit \"({char}*{digit}*)*\"
+
+%%
+int|float { printf("%s is a keyword\n", yytext); }
+"{" { printf("%s is an opening block\n", yytext); }
+"}" { printf("%s is a closing block\n", yytext); }
+"(" { printf("%s is an opening parenthesis\n", yytext); }
+")" { printf("%s is a closing parenthesis\n", yytext); }
+#.* { printf("%s is a preprocessor directive\n", yytext); }
+int|float|char|for|if { printf("%s is a keyword\n", yytext); }
+"+"|"-"|"/"|"*" { printf("%s is an arithmetic operator\n", yytext); }
+"<"|"<="|">"|">="|"!=" { printf("%s is a relational operator\n", yytext); }
+"=" { printf("%s is an assignment operator\n", yytext); }
+";"|"," { printf("%s is a special symbol\n", yytext); }
+{id} { printf("%s is an identifier\n", yytext); }
+{number} { printf("%s is a number\n", yytext); }
+"&&"|"||" { printf("%s is a logical operator\n", yytext); }
+{fun} { printf("%s is an identifier\n", yytext); }
+{lit} { printf("%s is a literal\n", yytext); }
+%%
+
+int main() {
+    FILE *fp;
+    fp = fopen("ccode.c", "r");
+    yyin = fp;
+    yylex();
+    fclose(fp);
+    return 0;
+}
+
+int yywrap() {
+    return 1;
+}
