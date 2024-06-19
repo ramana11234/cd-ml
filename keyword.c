eb526a793f1e838249dd8aa4aaf444571f2827d9
shvwxyz <155872247+shvwxyz18@users.noreply.github.com>
2024-06-19 21:03:19 +0530

    Create keywords.c

---
+++ keyword.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+void main() {
+    int i, flag = 0, m;
+    char s[5][10] = {"if", "else", "goto", "continue", "return"}, st[10];
+    printf("\n enter the string: ");
+    gets(st);
+
+    for (i = 0; i < 5; i++) {
+        m = strcmp(st, s[i]);
+        if (m == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0) {
+        printf("\n it is not a keyword");
+    } else {
+        printf("\n it is a keyword");
+    }
+
+    getch();
+}
