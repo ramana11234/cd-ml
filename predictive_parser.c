b8891f46876ee8422d3cd74a99325764678f7ae9
shvwxyz <155872247+shvwxyz18@users.noreply.github.com>
2024-07-10 19:23:24 +0530

    Update predictive_parser.c

--- predictive_parser.c
+++ predictive_parser.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-char prol[7][10] = { "S", "A", "A", "B", "B", "C", "C" };
-char pror[7][10] = { "A", "Bb", "Cd", "aB", "@", "Cc", "@" };
-char prod[7][10] = { "S->A", "A->Bb", "A->Cd", "B->aB", "B->@", "C->Cc", "C->@" };
-char first[7][10] = { "abcd", "ab", "cd", "a@", "@", "c@", "@" };
-char follow[7][10] = { "$", "$", "$", "a$", "b$", "c$", "d$" };
-char table[5][6][10];
+char prod[1][10] = { "S->aA" };
+char first[1][10] = { "a" };  
+char follow[1][10] = { "$" };
+char table[2][3][10];
 
 int numr(char c) {
     switch (c) {
@@ -14,81 +12,51 @@ int numr(char c) {
             return 0;
         case 'A':
             return 1;
-        case 'B':
-            return 2;
-        case 'C':
-            return 3;
         case 'a':
             return 0;
-        case 'b':
-            return 1;
-        case 'c':
-            return 2;
-        case 'd':
-            return 3;
         case '$':
-            return 4;
+            return 1;
     }
-    return (2);
+    return -1;
 }
 
 int main() {
-    int i, j, k;
-    
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 6; j++) {
+    int i, j;
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++) {
             strcpy(table[i][j], " ");
         }
     }
-    
-    printf("The following grammar is used for Parsing Table:\n");
-    for (i = 0; i < 7; i++) {
+
+    printf("The following grammar is used for the Parsing Table:\n");
+    for (i = 0; i < 1; i++) {
         printf("%s\n", prod[i]);
     }
-    
-    printf("\nPredictive parsing table:\n");
-    fflush(stdin); // This should be avoided, consider using alternative methods to clear input buffer.
-    
-    for (i = 0; i < 7; i++) {
-        k = strlen(first[i]);
-        for (j = 0; j < 10; j++) {
+
+    for (i = 0; i < 1; i++) {
+        for (j = 0; j < strlen(first[i]); j++) {
             if (first[i][j] != '@') {
-                strcpy(table[numr(prol[i][0]) + 1][numr(first[i][j]) + 1], prod[i]);
+                strcpy(table[numr('S') + 1][numr(first[i][j]) + 1], prod[i]);
             }
         }
     }
-    
-    for (i = 0; i < 7; i++) {
-        if (strlen(pror[i]) == 1) {
-            if (pror[i][0] == '@') {
-                k = strlen(follow[i]);
-                for (j = 0; j < k; j++) {
-                    strcpy(table[numr(prol[i][0]) + 1][numr(follow[i][j]) + 1], prod[i]);
-                }
-            }
-        }
-    }
-    
+
     strcpy(table[0][0], " ");
     strcpy(table[0][1], "a");
-    strcpy(table[0][2], "b");
-    strcpy(table[0][3], "c");
-    strcpy(table[0][4], "d");
-    strcpy(table[0][5], "$");
+    strcpy(table[0][2], "$");
     strcpy(table[1][0], "S");
-    strcpy(table[2][0], "A");
-    strcpy(table[3][0], "B");
-    strcpy(table[4][0], "C");
-    
-    printf("\n...................................................................\n");
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 6; j++) {
+
+    printf("\nPredictive parsing table:\n");
+    printf("\n.................................\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++) {
             printf("%-10s", table[i][j]);
-            if (j == 5) {
-                printf("\n.......................................................................\n");
+            if (j == 2) {
+                printf("\n.................................\n");
             }
         }
     }
-    
+
     return 0;
 }
